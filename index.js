ad9899a6bada72d4241bffdd07753c63a1df26c7
Vibodh <ghimab@gmail.com>
2025-10-30 21:35:46 +0000

    Testing Render

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
+
 // Initialize Shopify API
 Shopify.Context.initialize({
     API_KEY: process.env.VITE_SHOPIFY_API_KEY,
