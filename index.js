e03596f5d6e9132b72191904dd0a7a8dc7394b5f
Vibodh <ghimab@gmail.com>
2025-11-01 20:34:40 +0000

    Remove other imports

--- index.js
+++ index.js
@@ -78,10 +78,6 @@ app.use('/api/analytics', analyticsRoutes);
 app.use('/api/billing', billingRoutes);
 app.use('/api/webhooks', webhookRoutes);
 
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
