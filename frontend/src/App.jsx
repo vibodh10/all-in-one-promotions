74de7dd2cbe053453f2c7d5b6a1b0138b1283218
Vibodh <mvibodhk@gmail.com>
2026-02-21 15:19:45 +0000

    add /offers/:id/edit route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { AppProvider, Frame, Page } from "@shopify/polaris";
+import { AppProvider, Frame } from "@shopify/polaris";
 import "@shopify/polaris/build/esm/styles.css";
 import enTranslations from "@shopify/polaris/locales/en.json";
 import { MemoryRouter, Routes, Route } from "react-router-dom";
-import OfferBuilder from "./pages/OfferBuilder.jsx";
+
 import Dashboard from "./pages/Dashboard.jsx";
+import OfferBuilder from "./pages/OfferBuilder.jsx";
 import OfferList from "./pages/OfferList.jsx";
+import OfferEdit from "./pages/OfferEdit.jsx";
 import Analytics from "./pages/Analytics.jsx";
 import Settings from "./pages/Settings.jsx";
 
@@ -14,15 +16,14 @@ export default function App() {
         <AppProvider i18n={enTranslations}>
             <MemoryRouter initialEntries={["/"]}>
                 <Frame>
-                    <Page>
-                        <Routes>
-                            <Route path="/" element={<Dashboard />} />
-                            <Route path="/offers/new" element={<OfferBuilder />} />
-                            <Route path="/offers" element={<OfferList />} />
-                            <Route path="/analytics" element={<Analytics />} />
-                            <Route path="/settings" element={<Settings />} />
-                        </Routes>
-                    </Page>
+                    <Routes>
+                        <Route path="/" element={<Dashboard />} />
+                        <Route path="/offers" element={<OfferList />} />
+                        <Route path="/offers/new" element={<OfferBuilder />} />
+                        <Route path="/offers/:id/edit" element={<OfferEdit />} />
+                        <Route path="/analytics" element={<Analytics />} />
+                        <Route path="/settings" element={<Settings />} />
+                    </Routes>
                 </Frame>
             </MemoryRouter>
         </AppProvider>
