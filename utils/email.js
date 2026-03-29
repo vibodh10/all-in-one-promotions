dc91f8e6bc30b21955c03a730810a3a2f74af151
Vibodh <mvibodhk@gmail.com>
2026-03-29 19:27:34 +0100

    use resend properly

--- utils/email.js
+++ utils/email.js
@@ -4,11 +4,10 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendEmail(to, subject, html) {
 
-    await resend.email.send({
+    await resend.emails.send({
         from: "onboarding@resend.dev",
         to,
         subject,
         html
     });
-
 }
