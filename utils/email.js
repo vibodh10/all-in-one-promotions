6942491349dc81b32a11cbaa05ee4854e0af0791
Vibodh <mvibodhk@gmail.com>
2026-03-29 16:06:23 +0100

    send from proper sandboxing email

--- utils/email.js
+++ utils/email.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function sendEmail(to, subject, html) {
 
     await resend.email.send({
-        from: "hello@gl6.com",
+        from: "onboarding@resend.dev",
         to,
         subject,
         html
